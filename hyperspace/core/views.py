b558f09b464f3cf9abd55deed2c691665d621509
dogukanbasaran <dogukanbasarann@hotmail.com>
2025-03-15 16:50:27 +0300

    Lesson 9

--- hyperspace/core/views.py
+++ hyperspace/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from datetime import datetime
 from services.models import Service
+from blog.models import Blog
 
 def index(request):
     static_content = """
@@ -19,10 +19,12 @@ def index(request):
     """
 
     featured_services = Service.objects.filter(is_featured = True)
+    latest_posts = Blog.objects.all()[:3]
 
     context = {
         'static_content' : static_content, 
-        'featured_services': featured_services
+        'featured_services': featured_services,
+        'latest_posts': latest_posts
         }
     
     return render(request, 'core/index.html', context)
--- hyperspace/hyperspace/urls.py
+++ hyperspace/hyperspace/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('services/', include('services.urls'))
+    path('services/', include('services.urls')),
+    path('blog/', include('blog.urls'))
 ]
